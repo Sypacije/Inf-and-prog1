c0be3eb69a3d5f6105b86d58e38dcf5c71ac9cf4
Sypacije <146341322+Sypacije@users.noreply.github.com>
2023-10-02 22:51:08 +0400

    Add files via upload

---
+++ Задание 0.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    float a, b, p;
+    cin >> a >> b;
+    p = 2 * (a + b);
+    cout << "P = " << p << endl;
+    cout << "S = " << (a * b) << endl;
+    system("pause");
+    return 0;
+}
